323f38a7b2ebf579ec22d8396881fc96db14f022
Akash Thakur <akash0218.nitt@gmail.com>
2024-07-09 22:31:29 +0530

    error

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const axios = require('axios')
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
